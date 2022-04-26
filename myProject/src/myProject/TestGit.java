d07875f114de031a998faad7d5ba41715cbfac8d
M1082878 <M1082878@MT-5CG14786HZ>
2022-04-26 19:41:57 +0530

    added TestGit class

---
+++ myProject/src/myProject/TestGit.java
@@ -0,0 +1,12 @@
+package myProject;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Git");
+
+	}
+
+}
